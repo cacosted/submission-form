ac73fb9b8d6c073fdb4376514341bbadf7839067
Clemente Acosta <clems.acosta@gmail.com>
2022-09-03 11:37:22 -0500

    add mobile design, add custom font on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+          'regular': ['Josefin Sans', 'sans-serif']
+        }
+    },
   },
   plugins: [],
 }
